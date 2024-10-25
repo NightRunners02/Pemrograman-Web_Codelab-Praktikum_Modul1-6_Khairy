8c9b6af5fa3120347bcdb35e4469c4dbccae022a
NightRunners02 <148744891+NightRunners02@users.noreply.github.com>
2024-10-25 10:31:35 +0700

    Add files via upload

--- Codelab/index.php
+++ Codelab/index.php
@@ -2,9 +2,5 @@
 include "Controllers/ProductController.php";
 
 use Controller\ProductController;
-
-// Membuat objek ProductController
 $productController = new ProductController();
-
-// Memanggil method getAllProduct dan mencetak hasilnya
 echo $productController->getAllProduct();
